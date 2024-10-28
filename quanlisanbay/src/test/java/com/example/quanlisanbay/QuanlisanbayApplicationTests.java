4b49c5a22e1b87d2183472ee3ba0242ae7592302
Lê Anh Tú <leanhtu1123@gmail.com>
2024-10-29 00:34:15 +0700

    test bog 29.10

--- quanlisanbay/src/test/java/com/example/quanlisanbay/QuanlisanbayApplicationTests.java
+++ quanlisanbay/src/test/java/com/example/quanlisanbay/QuanlisanbayApplicationTests.java
@@ -2,12 +2,30 @@ package com.example.quanlisanbay;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import javax.sql.DataSource;
 
 @SpringBootTest
 class QuanlisanbayApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Đây là kiểm thử cơ bản để tải context
 	}
 
+	@TestConfiguration
+	static class TestConfig {
+
+		@Bean
+		@Primary
+		public DataSource dataSource() {
+			return new EmbeddedDatabaseBuilder()
+					.setType(EmbeddedDatabaseType.H2)
+					.build();
+		}
+	}
 }
